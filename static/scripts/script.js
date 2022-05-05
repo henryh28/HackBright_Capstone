a5cdee25abf0b3f3811aa5bf0633a5efd265bcba
Henry Hsu <h_hsu817@yahoo.com>
2022-05-04 21:03:40 -0700

    New user can create a new user account.  Can login with existing account and logout once logged in

---
+++ static/scripts/script.js
@@ -0,0 +1,15 @@
+
+
+
+
+// Navbar button to log user out
+document.querySelector("#btn-logout").addEventListener('click', () => {
+    window.location = "/logout";
+})
+
+
+// Navbar button to go to route "/"
+document.querySelector("#btn-home").addEventListener('click', () => {
+    window.location = "/";
+})
+
